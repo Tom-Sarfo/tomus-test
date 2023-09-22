17678e7ea153deef90a9b78874c74c7d39d6afd3
Tom-Sarfo <tomsarfodavis@gmail.com>
2023-09-22 05:14:11 +0000

    Align logo at the center of navbar - Implement for mobile devices only

--- src/Navigation/MobileNavBar.jsx
+++ src/Navigation/MobileNavBar.jsx
@@ -59,8 +59,9 @@ export default function MenuAppBar() {
 					>
 						<MenuIcon />
 					</IconButton>
+					<Box sx={{ flexGrow: 1 }} />
 					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-						Photos
+						Tomus
 					</Typography>
 					{auth && (
 						<div>
