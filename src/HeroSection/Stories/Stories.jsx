391b68c8437813285f80c79ca953356c8e8778ea
Tom-Sarfo <tomsarfodavis@gmail.com>
2023-09-26 05:09:50 +0000

    Amend size of stories and include share btn

--- src/HeroSection/Stories/Stories.jsx
+++ src/HeroSection/Stories/Stories.jsx
@@ -1,16 +1,19 @@
 import { Box } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+import "./Stories.css";
 
 export default function Stories() {
 	// let length = [1,2,3,4,5,6];
 	return (
-		<div>
+		<div className="Stories">
 			<Box
 				mt={2}
 				sx={{
 					width: 900,
-					height: 80,
+					height: 50,
 					display: "flex",
 					// backgroundColor: "grey",
 				}}
@@ -18,8 +21,8 @@ export default function Stories() {
 				<Stack direction="row" spacing={2}>
 					<Box
 						sx={{
-							width: 80,
-							height: 80,
+							width: 60,
+							height: 60,
 							backgroundColor: "white",
 							borderRadius: "50%",
 							display: "flex",
@@ -31,13 +34,13 @@ export default function Stories() {
 						<Avatar
 							alt="Remy Sharp"
 							src="https://cdn.pixabay.com/photo/2016/03/23/04/01/woman-1274056_1280.jpg"
-							sx={{ width: 70, height: 70 }}
+							sx={{ width: 50, height: 50 }}
 						/>
 					</Box>
 					<Box
 						sx={{
-							width: 80,
-							height: 80,
+							width: 60,
+							height: 60,
 							backgroundColor: "white",
 							borderRadius: "50%",
 							display: "flex",
@@ -49,13 +52,13 @@ export default function Stories() {
 						<Avatar
 							alt="Remy Sharp"
 							src="https://cdn.pixabay.com/photo/2016/10/15/23/52/foot-1744044_1280.jpg"
-							sx={{ width: 70, height: 70 }}
+							sx={{ width: 50, height: 50 }}
 						/>
 					</Box>
 					<Box
 						sx={{
-							width: 80,
-							height: 80,
+							width: 60,
+							height: 60,
 							backgroundColor: "white",
 							borderRadius: "50%",
 							display: "flex",
@@ -67,13 +70,13 @@ export default function Stories() {
 						<Avatar
 							alt="Remy Sharp"
 							src="https://cdn.pixabay.com/photo/2014/09/03/20/15/shoes-434918_1280.jpg"
-							sx={{ width: 70, height: 70 }}
+							sx={{ width: 50, height: 50 }}
 						/>
 					</Box>
 					<Box
 						sx={{
-							width: 80,
-							height: 80,
+							width: 60,
+							height: 60,
 							backgroundColor: "white",
 							borderRadius: "50%",
 							display: "flex",
@@ -85,13 +88,13 @@ export default function Stories() {
 						<Avatar
 							alt="Remy Sharp"
 							src="https://cdn.pixabay.com/photo/2016/10/16/13/44/young-woman-1745173_1280.jpg"
-							sx={{ width: 70, height: 70 }}
+							sx={{ width: 50, height: 50 }}
 						/>
 					</Box>
 					<Box
 						sx={{
-							width: 80,
-							height: 80,
+							width: 60,
+							height: 60,
 							backgroundColor: "white",
 							borderRadius: "50%",
 							display: "flex",
@@ -103,9 +106,10 @@ export default function Stories() {
 						<Avatar
 							alt="Remy Sharp"
 							src="/static/images/avatar/1.jpg"
-							sx={{ width: 70, height: 70 }}
+							sx={{ width: 50, height: 50 }}
 						/>
 					</Box>
+					<Button variant="contained" sx={{ bgcolor: "#872EB0"}}>Share your experience</Button>
 				</Stack>
 			</Box>
 		</div>
