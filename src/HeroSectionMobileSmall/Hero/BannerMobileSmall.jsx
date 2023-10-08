d91cd7964ed17b4d2824427fae66958f94220b61
Tom-Sarfo <tomsarfodavis@gmail.com>
2023-10-08 21:30:03 +0000

    Increase height of mobileBanner size 412px

--- src/HeroSectionMobileSmall/Hero/BannerMobileSmall.jsx
+++ src/HeroSectionMobileSmall/Hero/BannerMobileSmall.jsx
@@ -8,28 +8,34 @@ export default function Hero() {
 	return (
 		<div className="banner">
 			<div className="Tagline">
-			<Typography variant="h4" component="h2">
-				<b>
-					Craft <br />
-					that <br />
-					fit your lifestyle
-				</b>
-			</Typography><br />
-			<Button variant="contained" href="#contained-buttons" className="HeroBtn" sx={{bgcolor: "white", color: "#904B88"}}>
-				window shopping
-			</Button>
+				<Typography variant="h4" component="h2">
+					<b>
+						Craft <br />
+						that <br />
+						fit your lifestyle
+					</b>
+				</Typography>
+				<br />
+				<Button
+					variant="contained"
+					href="#contained-buttons"
+					className="HeroBtn"
+					sx={{ bgcolor: "white", color: "#904B88" }}
+				>
+					Do window shopping
+				</Button>
 			</div>
 			<Card
 				sx={{
 					width: "100%",
-					height: 450,
+					height: 600,
 					backgroundColor: "grey",
 				}}
 				className="BannerMobileSmall"
 			>
 				<CardMedia
 					component="img"
-					height="450"
+					height="600"
 					width="100%"
 					image="https://d2j6dbq0eux0bg.cloudfront.net/images/66306271/3355487565.jpg"
 					alt="Paella dish"
