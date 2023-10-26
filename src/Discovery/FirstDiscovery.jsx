9f77cf7460b87ae440e58436ff7cc2844e8a3675
Tom-Sarfo <tomsarfodavis@gmail.com>
2023-10-26 16:12:02 +0000

    Fix first discovery poor layout

--- src/Discovery/FirstDiscovery.jsx
+++ src/Discovery/FirstDiscovery.jsx
@@ -1,16 +1,37 @@
-import { Card, CardMedia } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 export default function FirstDiscovery() {
 	return (
-		<div>
-			<Card>
-				<CardMedia
-					component="img"
-					height="600"
-					image="https://d2j6dbq0eux0bg.cloudfront.net/images/66306271/3355513337.jpg"
-					alt="Birk Slippers"
+		<div className="FirstDiscovery">
+			<section className="FirstDisc1">
+				<img
+					src="https://d2j6dbq0eux0bg.cloudfront.net/images/66306271/3355513267.jpg"
+					alt="Tomus footwear"
 				/>
-			</Card>
+			</section>
+
+			<section className="FirstDisc2">
+				{/* use reusable components instead */}
+				<div className="FirstDisc2tagline">
+					<Typography variant="h2" component="h2">
+						<b>
+							Your <br />
+							Smile is our <br />
+							KPI
+						</b>
+					</Typography>
+					<br />
+					<Button
+						variant="contained"
+						href="#contained-buttons"
+						className="HeroBtn"
+						sx={{ bgcolor: "white", color: "#904B88" }}
+					>
+						Shop Now
+					</Button>
+				</div>
+			</section>
 		</div>
 	);
 }
