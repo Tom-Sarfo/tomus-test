239ee9382610fb053d3f86302eb875c64eea97c2
Tom-Sarfo <tomsarfodavis@gmail.com>
2023-12-28 13:26:50 +0000

    Add filter items feature

--- src/ProductCollection/FilterBar.jsx
+++ src/ProductCollection/FilterBar.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { ItemData } from "../Common/ItemCard/ItemCardData";
+import { Link } from "react-router-dom";
 
 export default function FilterBar() {
   return (
     <div className="filter">
-        <button className="filterBtn">Men</button>
-        <button className="filterBtn">Ladies</button>
-        <button className="filterBtn">Beads</button>
+      <Link to="/products/all">
+        <button className="filterBtn">All</button>
+      </Link>
+
+      <Link to="/products/birks"><button className="filterBtn">Birks</button></Link>
+      <Link to="/products/beads"><button className="filterBtn">Beads</button></Link>
     </div>
-  )
+  );
 }
--- src/ProductCollection/Products.jsx
+++ src/ProductCollection/Products.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import ProductCard from "./ProductCard";
 import FilterBar from "./FilterBar";
 import { ItemData } from "../Common/ItemCard/ItemCardData";
-import { Link } from "react-router-dom";
+import { Link, useParams} from "react-router-dom";
 
 export default function products() {
+  const type = useParams();
+  
+  //filter items
+  const items = ItemData.filter((item) => {
+    if (type.design === "beads") {
+      return item.type === "beads";
+    } else if(type.design === "birks"){
+      return item.type === "birks"
+    } else {
+      return ItemData;
+    } 
+  });
+
   return (
     <div className="">
       <FilterBar />
       <div className="product">
-        {ItemData.map((data, index) => (
+        {items.map((data, index) => (
           <ProductCard key={index} data={data} />
         ))}
       </div>
