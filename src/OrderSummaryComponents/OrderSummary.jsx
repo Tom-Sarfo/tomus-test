ca54f15d692c4f4438dc9a9e2d4333db192fae7a
Tom-Sarfo <tomsarfodavis@gmail.com>
2023-12-30 06:45:08 +0000

    Add scroll to top for order summary page

--- src/OrderSummaryComponents/OrderSummary.jsx
+++ src/OrderSummaryComponents/OrderSummary.jsx
@@ -1,8 +1,13 @@
 import Divider from "@mui/material/Divider";
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function OrderSummary() {
+  useEffect(() => {
+    // scroll to top on page load
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, []);
+
   const os = {
     inlineText: "flex justify-between text-gray-700",
   };
