a9144b99b39dcd08367d4e1ab056433cd73d9e7e
Tom-Sarfo <tomsarfodavis@gmail.com>
2024-09-12 12:35:01 +0000

    Change image source

--- src/MediaObjects.jsx
+++ src/MediaObjects.jsx
@@ -2,22 +2,22 @@ export const bannerMediaObject = {
   mediaSet: [
     {
       media: "(max-width: 320px)",
-      srcSet: "src/assets/Mobile-small-header.jpg 320w",
+      srcSet: "/Mobile-small-header.jpg 320w",
     },
     {
       media: "(max-width: 430px)",
-      srcSet: "src/assets/Mobile-header.jpg 430w",
+      srcSet: "/Mobile-header.jpg 430w",
     },
     {
       media: "(max-width: 640px)",
-      srcSet: "src/assets/tablet-mini.jpg 640w",
+      srcSet: "/tablet-mini.jpg 640w",
     },
     {
       media: "(max-width: 1024px)",
-      srcSet: "src/assets/tablet-max.jpg 1024w",
+      srcSet: "/tablet-max.jpg 1024w",
     },
   ],
-  img: "src/assets/Header-website.jpg",
+  img: "/Header-website.jpg",
   imgDesc: "African clogs",
   style: { width: "100%", height: "auto" },
 };
